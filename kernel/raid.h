103f4245c39d6f3aa0a13f5ec4291308dc7d10ce
murthymanideep <murthymanideep2006@gmail.com>
2026-05-08 21:23:01 +0530

    Add RAID subsystem header and shared storage definitions

---
+++ kernel/raid.h
@@ -0,0 +1,11 @@
+#ifndef RAID_H
+#define RAID_H
+
+#include "param.h"
+#define NUM_DISKS 4
+#define SIM_DISK_SIZE 10000
+#define SWAP_BLOCKS_PER_PAGE 4
+
+#define SWAP_START_BLOCK FSSIZE
+#define MAX_SWAP_PAGES 1024
+#endif
